d2ded64ecb4ff23f60344e655f55498c010836c0
mario sanz fernandez <mario.s_97@hotmail.com>
2020-11-23 20:31:55 +0100

    root

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Mayús+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+    print("-")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('App started')
+    main()
+
+
